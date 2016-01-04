1437464fe4297a30483431f8a7ce8f4165d99e97
davidretler <davidretler@gmail.com>
2016-01-03 20:45:28 -0500

    Still working, fixed memory lead

--- C/prob58.c
+++ C/prob58.c
@@ -1,33 +1,54 @@
 #include "prime_2.h"
 #include "stdio.h"
 #include "stdbool.h"
+#include "stdlib.h"
 
-#define SPIRAL_LAYERS 4
+//#define SPIRAL_LAYERS 4
 #define SPIRAL_MAX (SPIRAL_LAYERS*2-1)
 
+int SPIRAL_LAYERS;
+//int SPIRAL_MAX;
 
-
-long spiral[SPIRAL_MAX][SPIRAL_MAX];
-bool spiral_initialized = false;
-
-void init_spiral();
-void fill_spiral();
-void display_spiral();
-double fraction_prime_diagnals();
+long** init_spiral();
+void fill_spiral(long** spiral);
+void display_spiral(long** spiral);
+double fraction_prime_diagnals(long** spiral, int curr_layers);
+void free_spiral(long** spiral);
 
 int main() {
 
-	init_spiral();
-	fill_spiral();
+	puts("Initialzing...\n");
+	initialize_primes();
+	puts("Initialized\n\n");
 	
-	//display_spiral(); 
-	
-	printf("%lf%% are prime.\n", fraction_prime_diagnals()*100);
+	SPIRAL_LAYERS = 6;
 
+	long** spiral = init_spiral();
+	fill_spiral(spiral);
+
+	display_spiral(spiral);
+
+	for(int curr_layers = 2; curr_layers < SPIRAL_LAYERS; curr_layers++) {
+		
+		double fraction = fraction_prime_diagnals(spiral, curr_layers);
+	
+		printf("%d: %lf%% are prime.\n", curr_layers , fraction*100);
+		
+		if(fraction < 0.1) {
+			printf("%d\n", curr_layers);
+			break;
+		}
+	
+		
+	}
+	
+	free_spiral(spiral);
+	
+	
 	return 0;
 }
 
-void fill_spiral() {
+void fill_spiral(long** spiral) {
 	const int center_x = SPIRAL_MAX/2;
 	const int center_y = center_x;
 	
@@ -89,11 +110,8 @@ void fill_spiral() {
 	} 
 }
 
-void display_spiral() {
-	if(spiral_initialized == false) {
-		init_spiral();
-	}
-	
+void display_spiral(long** spiral) {
+
 	for(int y = 0; y < SPIRAL_MAX; y++) {
 		for(int x = 0; x < SPIRAL_MAX; x++) {
 			printf("%ld\t", spiral[x][y]);
@@ -102,30 +120,38 @@ void display_spiral() {
 	}
 }
 
-void init_spiral() {
+long** init_spiral() {
+
+	long** spiral = malloc(SPIRAL_MAX * sizeof(long*));
+	for(int i = 0; i < SPIRAL_MAX; i++) {
+		spiral[i] = malloc(SPIRAL_MAX * sizeof(long));
+	}
 	for(int y = 0; y < SPIRAL_MAX; y++) {
 		for(int x = 0; x < SPIRAL_MAX; x++) {
 			spiral[x][y] = 0;
 		}
 	}
-	spiral_initialized = true;
+	return spiral;
 }
 
-double fraction_prime_diagnals() {
-	if(spiral_initialized == false) {
-		init_spiral();
-	}
-	
+double fraction_prime_diagnals(long** spiral, int layers) {
+
+	int max = 2*layers - 1;
+	int center_x = max / 2;
+	int center_y = center_x;
+	int limit_low = center_x - (layers - 1);
+	int limit_high = center_x + (layers - 1);
+
 	long total = 0;
 	long primes = 0;
 	
-	for(int i = 0; i < SPIRAL_MAX; i++) {
+	for(int i = limit_low; i < limit_high; i++) {
 		// first diagnal
 		long n1 = spiral[i][i];
 		// second diagnal
 		long n2 = spiral[SPIRAL_MAX - i - 1][i];
 		
-		printf("(%ld, %ld)\n", n1, n2);
+		//printf("(%ld, %ld)\n", n1, n2);
 		
 		if(is_prime(n1)) {
 			primes++;
@@ -139,7 +165,14 @@ double fraction_prime_diagnals() {
 	}
 	total--; // subtract 1 because we counted the middle twice
 	
-	printf("%ld\n", primes);
+	//printf("%ld\n", primes);
 	
 	return ((double) primes)/((double) total);
+}
+
+void free_spiral(long** spiral) {
+	for(int i = 0; i < SPIRAL_MAX; i++) {
+		free(spiral[i]);
+	}
+	free(spiral);
 }
