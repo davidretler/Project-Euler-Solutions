05cb0a150c7c06687d7ef2f7442e382645a5b084
davidretler <davidretler@gmail.com>
2016-01-05 10:56:45 -0500

    Still Working on problem 54

--- python/p54.py
+++ python/p54.py
@@ -34,7 +34,21 @@ class Hand:
 		elif self.four_of_a_kind():
 			self.type = 'Four Of A Kind'
 			self.card_val = self.cards[1][0]
-			
+		elif self.full_house():
+			self.type = 'Full House'
+			self.card_val = self.cards[2][0]
+		elif self.flush():
+			self.type = 'Flush'
+			self.card_val = self.cards[0][0]
+		elif self.straight():
+			self.type = 'Straight'
+			self.card_val = self.cards[0][0]
+		elif self.three_of_a_kind():
+			self.type = 'Three Of A Kind'
+			self.card_val = self.cards[2][0]
+		elif self.two_pairs():
+			self.type = 'Two Pairs'
+			self.card_val = self.card_val_two_pairs()
 			
 						
 		
@@ -79,10 +93,51 @@ class Hand:
 	def full_house(self):
 		
 		# check if first 3 make 3 of a kind
+		if self.same_val(0, 3):
+			# check if last 2 are 2 of a kind
+			return self.same_val(3,5)
+		# check if last 3 are 3 of a kind
+		elif self.same_val(2,5):
+			# check if first 2 are two of a kind
+			return self.same_val(0,2)
+		else:
+			return False
+						
+	def flush(self):
 		
-		val = self.cards
+		return self.same_suit()
 		
+	def straight(self):
+		curr = CardValues[self.cards[0][0]]
+		for i in range(1,5):
+			curr = curr - 1
+			if CardValues[self.cards[i][0]] != curr:
+				return False
+		return True
+
+	def three_of_a_kind(self):
 		
+		if self.same_val(0,3):
+			return True
+		elif self.same_val(1,4):
+			return True
+		else:
+			return self.same_val(2,5)
+			
+	def two_pairs(self):
+	
+		if self.same_val(0,2):
+			if self.same_val(2,4):
+				return True
+			else:
+				return self.same_val(3,5)
+		else:
+			return self.same_val(1,3) and self.same_val(3,5)
+			
+	def one_pair(self):
+		
+		return self.same_val(0,2) or self.same_val(1,3) or self.same_val(2, 4) or self.same_val(3, 5) 
+						
 	# check if cards i through j have the same value
 	def same_val(self, i, j):
 		val = self.cards[i][0]
@@ -98,3 +153,20 @@ class Hand:
 			if self.cards[i][1] != suit:
 				return False
 		return True
+		
+	# gets the higher of the two values which form a pair
+	def card_val_two_pairs(self):
+		if self.same_val(0,2):
+			val1 = self.cards[0][0]
+			if self.same_val(2,4):
+				val2 = self.cards[2][0]
+			else:
+				val2 = self.cards[3][0]
+		else:
+			val1 = self.cards[1][0]
+			val2 = self.cards[3][0]
+			
+		if CardValues[val1] > CardValues[val2]:
+			return val1
+		else:
+			return val2
